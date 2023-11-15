dc5b1dd9174fd9fb59657846ed67ac6b736a0c33
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-15 13:10:29 +0530

    auth update

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,39 +2,35 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/UserModel');
 
 const protect = async (req, res, next) => {
-    let token;
+  let token;
 
-    // Check if the authorization header exists and starts with 'Bearer'
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-    ) {
-        token = req.headers.authorization.split(' ')[1];
+  // Check if the token is present in cookies
+  if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
+
+  // No token found
+  if (!token) {
+    return res.status(401).json({ msg: 'Not authorized, no token' });
+  }
+
+  // Token exists, proceed to verify it
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Add user data to the request object
+    req.user = await User.findById(decoded.id).select('-password');
+
+    next(); // Proceed to the next middleware
+  } catch (err) {
+    console.error(err);
+    if (err.name === 'JsonWebTokenError' && err.message === 'jwt malformed') {
+      return res.status(401).json({ msg: 'Malformed token. Please provide a valid token.' });
     }
-
-    
-    // No token found
-    if (!token) {
-        return res.status(401).json({ msg: 'Not authorized, no token' });
-    }
-
-    // Token exists, proceed to verify it
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
-        // Add user data to the request object
-        req.user = await User.findById(decoded.id).select('-password');
-
-        next(); // Proceed to the next middleware
-    } catch (err) {
-        console.error(err);
-        if (err.name === 'JsonWebTokenError' && err.message === 'jwt malformed') {
-            return res.status(401).json({ msg: 'Malformed token. Please provide a valid token.' });
-        }
-        return res.status(401).json({ msg: 'Not authorized, token failed' });
-    }
-}
+    return res.status(401).json({ msg: 'Not authorized, token failed' });
+  }
+};
 
 module.exports = {
-    protect
-}
+  protect
+};
