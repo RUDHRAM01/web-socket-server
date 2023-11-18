7980fb7545dd958106bd8a96584f598a94603db6
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-18 13:18:07 +0530

    index update

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const {notFound} = require('./middleware/errorMiddleware');
 const { errorHandler } = require('./middleware/errorMiddleware');
 
 
+
 require('dotenv').config();
 
 app.use(cors());
@@ -24,7 +25,26 @@ app.use('/api/chats', chatRouter);
 app.use('/api/message', messageRouter);
 app.use(notFound);
 app.use(errorHandler);
-app.listen(4000, () => {
+const server = app.listen(4000, () => {
     console.log('Server is running on port 4000');
     db();
+});
+
+const io = require('socket.io')(server, {
+    pingTimeout : 60000,
+    cors: {
+        origin: "http://localhost:3000",
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log("connected to socket.io");
+    socket.on("setup", userData => {
+        socket.join(userData.id);
+        console.log(userData.id);
+        socket.emit("connected");
+    });
+
+    socket.on("join chat", room => socket.join(room));
+
 });
