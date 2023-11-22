96391d83ad0ad158852ee998ba1fa8880bbce0a8
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-22 14:12:16 +0530

    update

--- index.js
+++ index.js
@@ -48,4 +48,13 @@ io.on('connection', (socket) => {
         
     });
 
+    socket.on("new message", (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+        if (!newMessageReceived.users) return console.log("Chat.users not defined");
+
+        newMessageReceived.users.forEach((user) => {
+            if (user._id === newMessageReceived.sender._id) return;
+            socket.in(user._id).emit("message received", newMessageReceived);
+        })
+    })
 });
