16461b0127a7986e7481304e5ab4499cabe96ca7
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-22 14:12:22 +0530

    update

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -6,20 +6,24 @@ const Message = require('../models/MessageModel');
 const sendMessage = asyncHandler(async (req, res) => {
     const { message, chatId } = req.body;
     const userId = req.user._id
-    const chat = await Chat.findById(chatId);
+    const chat = await Chat.findById(chatId).populate('users');
     if (!chat) {
         return res.status(404).json({ msg: 'Chat not found' });
     }
     if (!message) {
         return res.status(400).json({ msg: 'Message is required' });
     }
-    const newMessage = await Message.create({
+    let newMessage = await Message.create({
         sender: userId,
         chat: chatId,
         content : message
     });
     chat.latestMessage = newMessage._id;
     await chat.save();
+    newMessage = {
+        ...newMessage._doc,
+        users : chat.users
+    }
     console.log(newMessage, "new message")
     res.status(201).json({ msg: 'Message sent successfully', newMessage });
 });
