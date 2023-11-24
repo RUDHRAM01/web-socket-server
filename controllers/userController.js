3dda91d8f5772941385924439e2631b68bff69cf
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-24 21:32:42 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,9 @@ const Users = require('../models/UserModel');
 const bcrypt = require('bcrypt');
 const cookie = require('cookie');
 const { generateToken } = require('../db/token');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
 
 
 // const register = async (req, res) => {
@@ -9,13 +12,13 @@ const { generateToken } = require('../db/token');
 //     if (!email || !password) {
 //       return res.status(400).json({ msg: 'Please enter all fields' });
 //     }
-  
+
 //     if (password.length < 6) {
 //       return res
 //         .status(400)
 //         .json({ msg: 'Password must be at least 6 characters' });
 //     }
-  
+
 //     try {
 //       const re = await Users.find({ email });
 //       if (re.length > 0)
@@ -26,12 +29,12 @@ const { generateToken } = require('../db/token');
 //           email,
 //           password: hashPass,
 //         });
-  
+
 //         try {
 //           const savedUser = await newUser.save();
-  
+
 //           const token = generateToken(savedUser._id);
-  
+
 //           // Set the token as a cookie
 //           res.setHeader(
 //             'Set-Cookie',
@@ -41,7 +44,7 @@ const { generateToken } = require('../db/token');
 //               sameSite: 'strict',
 //             })
 //           );
-  
+
 //           res.status(200).json({
 //             user: {
 //               id: savedUser._id,
@@ -56,15 +59,15 @@ const { generateToken } = require('../db/token');
 //       res.status(400).json({ err });
 //     }
 //   };
-  
+
 //   const login = async (req, res) => {
 //     const { email, password } = req.body;
 //     console.log(email, password);
-  
+
 //     if (!email || !password) {
 //       return res.status(400).json({ msg: 'Please enter all fields' });
 //     }
-  
+
 //     try {
 //       const re = await Users.find({ email });
 //       if (re.length === 0) return res.status(400).json('User does not exist');
@@ -74,9 +77,9 @@ const { generateToken } = require('../db/token');
 //           return res.status(400).json({ msg: 'Invalid credentials' });
 //         else {
 //           console.log('d');
-  
+
 //           const token = generateToken(re[0]._id);
-  
+
 //           // Set the token as a cookie
 //           res.setHeader(
 //             'Set-Cookie',
@@ -86,7 +89,7 @@ const { generateToken } = require('../db/token');
 //               sameSite: 'strict',
 //             })
 //           );
-  
+
 //           res.status(200).json({
 //             user: {
 //               id: re[0]._id,
@@ -99,103 +102,170 @@ const { generateToken } = require('../db/token');
 //       console.log(err);
 //     }
 //   };
-  
+
+const sendVerifyMail = (email, id) => {
+    console.log(process.env.EMAIL_PASSWORD, " ", email, " ", id);
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: false,
+                requireTLS: true,
+                user: 'rudhramsaraswat1112@gmail.com',
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+        const mailOptions = {
+            from: 'rudhramsaraswat1112@gmail.com',
+            to: email,
+            subject: 'Verify your email',
+            html: `
+<body>
+    <table width="600" cellpadding="0" cellspacing="0" style="margin:auto; background-color:#f8f9fa; border:1px solid #e0e0e0; padding:20px;">
+        <tr>
+            <td style="text-align:center; padding:20px; background-color:#ffffff;">
+                <img src="" alt="Logo" style="max-width:100%; height:auto;">
+            </td>
+        </tr>
+        <tr>
+            <td style="padding:20px; background-color:#ffffff;">
+
+                <h1 style="font-size:24px; font-family: Arial, sans-serif; color:#333333;">Welcome to Our Service!</h1>
+                <p style="font-size:16px; font-family: Arial, sans-serif; color:#666666; line-height:1.5;">Thank you for signing up for our service. We are excited to have you on board!</p>
+                <a href="http://localhost:4000/api/auth/verify?id=${id}" style="display:inline-block; background-color:#28a745; color:#ffffff; text-decoration:none; padding:10px 20px; margin-top:20px; border-radius:4px; font-family: Arial, sans-serif;">Verify Email</a>
+            </td>
+        </tr>
+        
+        <tr>
+            <td style="text-align:center; padding:20px; background-color:#e9ecef;">
+                <p style="font-size:14px; font-family: Arial, sans-serif; color:#666666;">© 2023 CA. All Rights Reserved.</p>
+                <p style="font-size:14px; font-family: Arial, sans-serif; color:#666666;">Rudhram Saraswat</p>
+            </td>
+        </tr>
+    </table>
+</body>`
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent successfully: ' + info.response);
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
+    if (!email || !name || !password) {
+        return res.status(400).json({ msg: 'Please enter all fields' });
+    }
 
-    
-  if (!email || !password || !name) {
-      return res.status(400).json({ msg: 'Please enter all fields' });
-  }
+    const regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/;
+    if (!regex.test(password)) {
+        return res.status(400).json({ msg: 'Password must be at least 6 characters and should contain a number and a special character' });
+    }
 
-  if (password.length < 6) {
-      return res
-          .status(400)
-          .json({ msg: 'Password must be at least 6 characters' });
-  }
+    try {
+        const re = await Users.find({ email });
+        if (re.length > 0)
+            return res.status(400).json({ msg: 'User already exists' });
+        else {
+            const newUser = new Users({
+                name,
+                email,
+                password : await bcrypt.hash(password, 10),
+            });
 
-  try {
-      const re = await Users.find({ email });
-      if (re.length > 0)
-          return res.status(400).json({ msg: 'User already exists' });
-      else {
-          const hashPass = await bcrypt.hash(password, 10);
-          const newUser = new Users({
-              name,
-              email,
-              password : hashPass
-          });
-         
-          
-          try {
-              const savedUser = await newUser.save();
-              res.status(200).json(
-                  {
-                      user: {
-                          id: savedUser._id,
-                          email: savedUser.email,
-                          token: generateToken(savedUser._id)
-                      }
-                  }
-              );
-          } catch (err) {
-              res.status(400).json({ err });
-          }
-         
-      }
-  } catch (err) {
-      res.status(400).json({ err });
-  }
+            try {
+                const savedUser = await newUser.save();
+                sendVerifyMail(email, savedUser._id);
+                res.status(200).json({ msg: "Account Created! Please verify your email" });
+            } catch (err) {
+                res.status(400).json({ err });
+            }
+        }
+    } catch (err) {
+        res.status(400).json({ err });
+    }
+};
+
+const verify = async (req, res) => {
+
+    const { id } = req.query;
+    try {
+        const re = await Users.find({ _id: id });
+        if (re.length === 0) {
+            return res.status(400).json({ msg: 'User does not exist' });
+        } else {
+            try {
+                const updatedUser = await Users.findByIdAndUpdate(id, { isAuthenticated: true });
+                res.redirect(`http://localhost:3000/login`);
+            } catch (err) {
+                res.status(400).json({ msg: "unknown" });
+            }
+        }
+    } catch (err) {
+        res.status(400).json({ msg: "unknown" });
+    }
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  
-  if (!email || !password) {
-      return res.status(400).json({ msg: 'Please enter all fields' });
-  }
 
-  try {
-      const re = await Users.find({ email });
-      if (re.length === 0)
-          return res.status(400).json('User does not exist');
-      else {
-          const isMatch = await bcrypt.compare(password, re[0].password);
-          if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
-          else {
-              res.status(200).json(
-                  {
-                      user: {
-                          id: re[0]._id,
-                          email: re[0].email,
-                          name: re[0].name,
-                          profilePic: re[0].profilePic,
-                          token: generateToken(re[0]._id)
-                      }
-                  }
-                  
-              );
-          }
-      }
-  } catch (err) {
-      res.status(400).json({ err });
-  }
+    if (!email || !password) {
+        return res.status(400).json({ msg: 'Please enter all fields' });
+    }
+
+    try {
+        const re = await Users.find({ email });
+        if (!re[0].isAuthenticated) {
+            return res.status(400).json({ msg: "Please verify your email" })
+        }
+        if (re.length === 0)
+            return res.status(400).json({ msg: 'Invalid credentials'});
+        else {
+            const isMatch = await bcrypt.compare(password, re[0].password);
+            if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+            else {
+                res.status(200).json(
+                    {
+                        user: {
+                            id: re[0]._id,
+                            email: re[0].email,
+                            name: re[0].name,
+                            profilePic: re[0].profilePic,
+                            token: generateToken(re[0]._id)
+                        }
+                    }
+
+                );
+            }
+        }
+    } catch (err) {
+        res.status(400).json({ err });
+    }
 };
 
 
 // /api/users?search=name
 const searchUser = async (req, res) => {
-  const { search } = req.query;
-  let result = [];
-  if (search) {
-    result = await Users.find({
-      name: { $regex: search, $options: 'i' },
-    }).select('-password');
-  } else {
-    result = await Users.find({}).select('-password');
-  }
+    const { search } = req.query;
+    let result = [];
+    if (search) {
+        result = await Users.find({
+            name: { $regex: search, $options: 'i' },
+        }).select('-password');
+    } else {
+        result = await Users.find({}).select('-password');
+    }
     res.status(200).json(result);
 };
 
@@ -203,12 +273,12 @@ const searchUser = async (req, res) => {
 const allUsers = async (req, res) => {
     try {
         // in this it should return all the users except the logged in user
-    const users = await Users.find({ _id: { $ne: req.user._id } }).select('-password');
-      
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+        const users = await Users.find({ _id: { $ne: req.user._id } }).select('-password');
+
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 };
 
 
@@ -216,6 +286,7 @@ module.exports = {
     register,
     login,
     searchUser,
-    allUsers
+    allUsers,
+    verify
 };
 
