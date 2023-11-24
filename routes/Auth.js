c7c4d882ab869eaa87dd7eadeef3267e742147a5
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-24 21:36:25 +0530

    limiter added

--- routes/Auth.js
+++ routes/Auth.js
@@ -2,9 +2,19 @@
 
 const router = require('express').Router();
 const userController = require('../controllers/userController');
+const rateLimit = require('express-rate-limit');
 
+const Limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 10 requests per windowMs
+  handler: (req, res) => {
+    res.status(429).json({
+      error: 'Too many attempts from this IP, please try again later.',
+    });
+  },
+});
 
-router.get("/verify", userController.verify);
+router.get("/verify",Limiter, userController.verify);
 
 
 module.exports = router;
