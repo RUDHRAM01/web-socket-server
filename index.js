84141a4b6e488953da1a162249c07ad0caf4d9e5
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-26 20:57:09 +0530

    update

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const server = app.listen(4000, () => {
 const io = require('socket.io')(server, {
     pingTimeout : 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: allowedOrigins,
     }
 });
 
