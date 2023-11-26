51cf3acd15baa6465717676afe10972896414a79
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-26 20:46:17 +0530

    ok

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,19 +2,21 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/UserModel');
 
 const protect = async (req, res, next) => {
-    // if mode is developement then skip the auth
-    // if (process.env.NODE_ENV === 'development') {
-    //     return next();
-    // }
+  // Uncomment the following lines if you want to skip authentication in development
+  // if (process.env.NODE_ENV === 'development') {
+  //   return next();
+  // }
+
   let token;
+
   // Check if the token is present in cookies
   if (req.cookies && req.cookies.token) {
     token = req.cookies.token;
   }
-  console.log(token)
+
   // No token found
   if (!token) {
-    return res.status(401).json({ msg: 'Not authorized' });
+    return res.status(401).json({ msg: 'Not authorized. Token not found.' });
   }
 
   // Token exists, proceed to verify it
@@ -23,14 +25,19 @@ const protect = async (req, res, next) => {
 
     // Add user data to the request object
     req.user = await User.findById(decoded.id).select('-password');
-    console.log(req.user)
+    
+    // Uncomment the following line if you want to log user details
+    // console.log(req.user);
+
     next(); // Proceed to the next middleware
   } catch (err) {
     console.error(err);
+
     if (err.name === 'JsonWebTokenError' && err.message === 'jwt malformed') {
       return res.status(401).json({ msg: 'Malformed token. Please provide a valid token.' });
     }
-    return res.status(401).json({ msg: 'Not authorized, token failed' });
+
+    return res.status(401).json({ msg: 'Not authorized. Token verification failed.' });
   }
 };
 
