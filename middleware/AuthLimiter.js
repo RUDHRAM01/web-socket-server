1f046cdf1a48a1fd49728a439f2eb89c4bbcb47b
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-20 17:53:54 +0530

    update

---
+++ middleware/AuthLimiter.js
@@ -0,0 +1,13 @@
+const rateLimit = require('express-rate-limit');
+
+const limitTracker = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 1000, // Limit each IP to 10 requests per windowMs
+    handler: (req, res) => {
+        res.status(429).json({
+            msg: 'Too many requests from this IP, please try again later.',
+        });
+    },
+});
+
+module.exports = limitTracker;
