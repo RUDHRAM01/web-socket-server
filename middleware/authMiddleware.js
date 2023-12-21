c6d50c61170814ef7814726437c4f845635dadee
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-21 13:42:02 +0530

    adding high security

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,90 +1,91 @@
-// const jwt = require('jsonwebtoken');
-// const User = require('../models/UserModel');
+const jwt = require('jsonwebtoken');
+const User = require('../models/UserModel');
+const {HeaderChecker} = require('./HeadersChecker');
 
-// const protect = async (req, res, next) => {
-//   // Uncomment the following lines if you want to skip authentication in development
-//   // if (process.env.NODE_ENV === 'development') {
-//   //   return next();
-//   // }
 
-//   let token;
+const protect = async (req, res, next) => {
+    if (process.env.NODE_ENV === 'development') {
+      try {
+        let token;
+  
+        if (req.cookies && req.cookies.xxrsr) {
+          token = req.cookies.xxrsr;
+  
+          if (!token) {
+            return res.status(401).json({ msg: 'Not authorized. Token not found.' });
+          }
+  
+          const decoded = jwt.verify(token, process.env.JWT_SECRET);
+          req.user = await User.findById(decoded.id).select('-password');
+          next();
+        } else {
+          // You may want to handle the case where the cookie is not present.
+          return res.status(401).json({ msg: 'Not authorized. Token not provided.' });
+        }
+      } catch (err) {
+        console.error(err);
+  
+        if (err.name === 'JsonWebTokenError' && err.message === 'jwt malformed') {
+          return res.status(401).json({ msg: 'Malformed token. Please provide a valid token.' });
+        }
+  
+        return res.status(401).json({ msg: 'Not authorized. Token verification failed.' });
+      }
+    } else {
+      try {
+        // Assuming HeaderChecker is a middleware function
+        HeaderChecker(req, res, next);
+      } catch (err) {
+        return res.status(401).json({ msg: 'Something went wrong.' });
+      }
+    }
+  };
+  
 
-//   // Check if the token is present in cookies
-//   if (req.cookies && req.cookies.token) {
-//     token = req.cookies.token;
-//   }
-
-//   // No token found
-//   if (!token) {
-//     return res.status(401).json({ msg: 'Not authorized. Token not found.' });
-//   }
-
-//   // Token exists, proceed to verify it
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//     // Add user data to the request object
-//     req.user = await User.findById(decoded.id).select('-password');
-    
-//     // Uncomment the following line if you want to log user details
-//     // console.log(req.user);
-
-//     next(); // Proceed to the next middleware
-//   } catch (err) {
-//     console.error(err);
-
-//     if (err.name === 'JsonWebTokenError' && err.message === 'jwt malformed') {
-//       return res.status(401).json({ msg: 'Malformed token. Please provide a valid token.' });
-//     }
-
-//     return res.status(401).json({ msg: 'Not authorized. Token verification failed.' });
-//   }
-// };
-
-// module.exports = {
-//   protect
-// };
+module.exports = {
+  protect
+};
 
 
 // way 2
 
-const jwt = require('jsonwebtoken');
-const User = require('../models/UserModel');
+// const jwt = require('jsonwebtoken');
+// const User = require('../models/UserModel');
 
-const protect = async (req, res, next) => {
-    let token;
+// const protect = async (req, res, next) => {
+//     let token;
 
-    // Check if the authorization header exists and starts with 'Bearer'
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-    ) {
-        token = req.headers.authorization.split(' ')[1];
-    }
+//     // Check if the authorization header exists and starts with 'Bearer'
+//     if (
+//         req.headers.authorization &&
+//         req.headers.authorization.startsWith('Bearer')
+//     ) {
+//         token = req.headers.authorization.split(' ')[1];
+//     }
 
     
-    // No token found
-    if (!token) {
-        return res.status(401).json({ msg: 'Not authorized, no token' });
-    }
+//     // No token found
+//     if (!token) {
+//         return res.status(401).json({ msg: 'Not authorized, no token' });
+//     }
 
-    // Token exists, proceed to verify it
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     // Token exists, proceed to verify it
+//     try {
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         
-        // Add user data to the request object
-        req.user = await User.findById(decoded.id).select('-password');
+//         // Add user data to the request object
+//         req.user = await User.findById(decoded.id).select('-password');
         
-        next(); // Proceed to the next middleware
-    } catch (err) {
-        console.error(err);
-        if (err.name === 'JsonWebTokenError' && err.message === 'jwt malformed') {
-            return res.status(401).json({ msg: 'Malformed token. Please provide a valid token.' });
-        }
-        return res.status(401).json({ msg: 'Not authorized, token failed' });
-    }
-}
+//         next(); // Proceed to the next middleware
+//     } catch (err) {
+//         console.error(err);
+//         if (err.name === 'JsonWebTokenError' && err.message === 'jwt malformed') {
+//             return res.status(401).json({ msg: 'Malformed token. Please provide a valid token.' });
+//         }
+//         return res.status(401).json({ msg: 'Not authorized, token failed' });
+//     }
+// }
 
-module.exports = {
-    protect
-}
+// module.exports = {
+//     protect
+// }
