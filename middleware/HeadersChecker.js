1ea55a66a7d111265fe8574c860b3e72eae41c4b
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-21 14:33:06 +0530

    removing consoles

--- middleware/HeadersChecker.js
+++ middleware/HeadersChecker.js
@@ -3,27 +3,22 @@ const HeadersChecker = (req, res, next) => {
     if (process.env.NODE_ENV === 'development') return next();
     
     if (!req.headers['x-app-type']) {
-        console.log("error 1");
         throw new Error('app type header not present');
         }
         
     if (req.headers['x-app-type'] !== 'RsRequest') {
-        console.log("error 2");
         throw new Error('app type header not valid');
         }
     
     if (!req.headers['x-request-id']) {
-        console.log("error 3");
         throw new Error('request id header not present');
         }
     
     if (!req.headers['x-hashed-id']) {
-        console.log("error 4");
         throw new Error('hashed id header not present');
         }
     
     if (!req.headers['x-request-token']) {
-        console.log("error 5");
         throw new Error('request token header not present');
         }
     
@@ -32,9 +27,7 @@ const HeadersChecker = (req, res, next) => {
         const requestToken = req.headers['x-request-token'];
         const bytes = CryptoJS.AES.decrypt(hashedId, requestToken);
         const tok = bytes.toString(CryptoJS.enc.Utf8);
-        console.log("error middle");
     if (requestId !== tok) {
-        console.log("error 6");
             throw new Error('hashed id header not valid');
     }
     next();
