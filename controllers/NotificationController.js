6c5608aadc1db1c7fcb9dac7b2bc0fc6d872c18a
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-01-26 15:56:32 +0530

    adding noti cont

---
+++ controllers/NotificationController.js
@@ -0,0 +1,21 @@
+const Notification = require('../models/NotificationModel');
+
+const getNotifications = async (req, res) => {
+    try {
+        const notifications = await Notification.find({ for: req.user._id, seen: false }).populate('from', 'name profilePic');
+        res.status(200).json( notifications );
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+}  
+
+const deleteNotification = async (req, res) => {
+    try {
+        await Notification.findByIdAndUpdate(req.params.id, { seen: true });
+        res.status(200).json({ msg: "Notification deleted" });
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+}
+
+module.exports = { getNotifications, deleteNotification };
